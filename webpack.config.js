d6e83a5ffe78c44eba1f51eb567524a05d99ad46
Patrick <patrickhu91@gmail.com>
2023-03-21 16:08:47 -0700

    boilerplate setup

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html")
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx|ts|tsx)$/,
+        exclude: '/node_modules/',
+        use: ['babel-loader'],
+      },
+      // {
+      //   test: /\.s?[ac]ss$/,
+      //   use: ['style-loader', 'css-loader', 'sass-loader'],
+      // },
+      // {
+      //   test: /\.(png|jpe?g|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: 'file-loader'
+      //     }
+      //   ]
+      // }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  devServer: {
+    historyApiFallback: true,
+  }
+}
