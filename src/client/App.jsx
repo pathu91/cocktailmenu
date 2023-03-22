a73007affcac691395cb8d2cd96102c52372e4c6
Patrick <patrickhu91@gmail.com>
2023-03-22 11:00:15 -0700

    import stylesheet to application

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import {
+    createBrowserRouter,
+    RouterProvider,
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom"
 
+import { NavBar } from "./containers/NavBar"
+import Test from "./containers/test"
+import Burrito from "./containers/burrito"
+import Home from "./containers/Home"
 const App = () => {
-  return(
-    <h1>
-      Hello world!
-    </h1>
-  )
+    return (
+        <>
+            <Router>
+                <NavBar />
+                <Routes>
+                    <Route path="/abc" element={<Test />} />
+                    <Route path="/xyz" element={<Burrito />} />
+                </Routes>
+            </Router>
+            <Home />
+        </>
+    )
 }
 
-export default App;
+export default App
