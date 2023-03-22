55d7fc9011777d6e3df2c74aeffe3f445e3ab00c
Patrick <patrickhu91@gmail.com>
2023-03-22 13:44:02 -0700

    copy-webpack-plugin webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,51 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const path = require("path")
 
 module.exports = {
-  entry: './src/client/index.jsx',
+  entry: "./src/client/index.jsx",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public/index.html")
-    })
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public"),
+          to: "public",
+        },
+      ],
+    }),
   ],
   module: {
     rules: [
       {
         test: /.(js|jsx|ts|tsx)$/,
-        exclude: '/node_modules/',
-        use: ['babel-loader'],
+        exclude: "/node_modules/",
+        use: ["babel-loader"],
       },
       {
         test: /\.s?[ac]ss$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
-      // {
-      //   test: /\.(png|jpe?g|gif)$/i,
-      //   use: [
-      //     {
-      //       loader: 'file-loader'
-      //     }
-      //   ]
-      // }
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
   devServer: {
     historyApiFallback: true,
-  }
+  },
 }
