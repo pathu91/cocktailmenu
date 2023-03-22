20be0f47c3f1e2f78129c7cfb5bb804f36dfaf48
Patrick <patrickhu91@gmail.com>
2023-03-22 13:43:06 -0700

    add featured cocktail component

---
+++ src/client/components/FeaturedCocktail.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const FeaturedCocktail = () => {
+  return (
+    <div className="cocktail-function">
+      <img
+        src="public/images/old-fashioned.jpeg"
+        alt="old-fashioned-whiskey"
+      ></img>
+      <h2>Old Fashioned</h2>
+      <button>
+        <p>See Recipe</p>
+      </button>
+    </div>
+  )
+}
+
+export default FeaturedCocktail
