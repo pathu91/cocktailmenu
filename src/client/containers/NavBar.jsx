f5aafa6052871295dc1b3410619df45e9f13cb78
Patrick <patrickhu91@gmail.com>
2023-03-22 12:36:56 -0700

    add social links and hamburger placement elements

--- src/client/containers/NavBar.jsx
+++ src/client/containers/NavBar.jsx
@@ -1,22 +1,25 @@
 import { Link } from "react-router-dom"
 export const NavBar = () => {
-    return (
-        // <Fragment>
-        <div id="nav-bar-container">
-            <div id="nav-links">
-                <li>
-                    <Link to="/abc">Test</Link>
-                </li>
-                <li>
-                    <Link to="/xyz">Burritos</Link>
-                </li>
-            </div>
-            <div id="nav-socials">
-                <li ahref="#">FB</li>
-                <li ahref="#">IG</li>
-                <li ahref="#">TW</li>
-            </div>
-        </div>
-        // </Fragment>
-    )
+  return (
+    // <Fragment>
+    <div id="nav-bar-container">
+      <div id="nav-links">
+        <li>
+          <Link to="/abc">Test</Link>
+        </li>
+        <li>
+          <Link to="/xyz">Burritos</Link>
+        </li>
+      </div>
+      <div id="nav-socials">
+        <li ahref="#">FB</li>
+        <li ahref="#">IG</li>
+        <li ahref="#">TW</li>
+        <li id="burger-menu" ahref="#">
+          BG
+        </li>
+      </div>
+    </div>
+    // </Fragment>
+  )
 }
