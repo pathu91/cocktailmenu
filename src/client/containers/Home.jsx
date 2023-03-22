59f8271c833dbf7367c1d4327b34675ea767e733
Patrick <patrickhu91@gmail.com>
2023-03-22 12:36:37 -0700

    add remaining home page elements

--- src/client/containers/Home.jsx
+++ src/client/containers/Home.jsx
@@ -1,22 +1,33 @@
 import React from "react"
 
 const Home = () => {
-    return (
-        <div id="mainHome">
-            <div id="featured-container">
-                <div id="featured-cocktail">
-                    <h3>Lorem ipsum something possum</h3>
-                    <button>Go to the possum</button>
-                </div>
-                <div id="featured-accent-box">
-                    <h3>peanut</h3>
-                </div>
-            </div>
-            <div>
-                <h2>Our favorite cocktails</h2>
-            </div>
+  return (
+    <div id="mainHome">
+      <div id="featured-container">
+        <div id="featured-cocktail">
+          <h3>Lorem ipsum something possum</h3>
+          <button>Go to the possum</button>
         </div>
-    )
+        <div id="featured-accent-box">
+          <h3>peanut</h3>
+        </div>
+      </div>
+      <div id="featured-cocktail-button-container">
+        <h2>Our favorite cocktails</h2>
+      </div>
+      <div id="find-your-cocktails-container">
+        <div className="cocktail-function">
+          <h1>Search By Name</h1>
+        </div>
+        <div className="cocktail-function">
+          <h1>Search By Ingredient</h1>
+        </div>
+        <div className="cocktail-function">
+          <h1>Surprise Me</h1>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
