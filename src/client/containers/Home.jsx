7157f1b5a7439dbae53677402469f996dd4eb0fd
Patrick <patrickhu91@gmail.com>
2023-03-27 09:42:13 -0700

    created component for featured cocktail to modularize homepage

--- src/client/containers/Home.jsx
+++ src/client/containers/Home.jsx
@@ -1,33 +1,11 @@
-import React, { Fragment } from "react"
-import FeaturedCocktail from "../components/FeaturedCocktail"
-
+import React from "react"
+import CocktailSearch from "./CocktailSearch"
+import HomePageBanner from "../components/homeWelcome/HomePageBanner"
 const Home = () => {
   return (
     <div id="mainHome">
-      <div id="featured-container">
-        <div id="featured-cocktail">
-          <h3>Lorem ipsum something possum</h3>
-          <button>Go to the possum</button>
-        </div>
-        <div id="featured-accent-box">
-          <h3>peanut</h3>
-        </div>
-      </div>
-      <div id="featured-cocktail-button-container">
-        <h2>Our favorite cocktails</h2>
-      </div>
-      <Fragment>
-        <FeaturedCocktail />
-        <div className="cocktail-function">
-          <h1>Search By Name</h1>
-        </div>
-        <div className="cocktail-function">
-          <h1>Search By Ingredient</h1>
-        </div>
-        <div className="cocktail-function">
-          <h1>Surprise Me</h1>
-        </div>
-      </Fragment>
+      <HomePageBanner />
+      <CocktailSearch />
     </div>
   )
 }
